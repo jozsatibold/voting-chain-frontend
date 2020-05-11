596cbd8cac32005827b63ca1a95c742bec3415d7
Jozsa Tibold <jozsatibold@yahoo.com>
2020-05-11 09:59:30 +0300

    Global module fix

--- src/modules/global/global.module.ts
+++ src/modules/global/global.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { NotificationComponent } from "../shared/components/notification/notification.component";
-import { PageNotFoundComponent } from "../shared/components/page-not-found/page-not-found.component";
-import { SpinnerComponent } from "../shared/components/spinner/spinner.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { SharedModule } from "../shared/shared.module";
 import { TranslateModule } from "@ngx-translate/core";
@@ -11,11 +8,6 @@ import { GlobalEffects } from "./+state/global.effects";
 import { StoreModule } from "@ngrx/store";
 import { globalReducer } from "./+state/global.reducer";
 
-const components = [
-  NotificationComponent,
-  PageNotFoundComponent,
-  SpinnerComponent
-];
 
 @NgModule({
   imports: [
@@ -27,7 +19,7 @@ const components = [
     StoreModule.forFeature("global", globalReducer),
     EffectsModule.forFeature([GlobalEffects])
   ],
-  declarations: [...components],
-  exports: [...components]
+  declarations: [],
+  exports: []
 })
 export class GlobalModule {}
